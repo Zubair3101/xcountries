37c4edf4f6289f74dc8be2b76d287f5772381224
Zubair Ansari <ansarizubair3101@gmail.com>
2025-05-07 15:55:53 +0530

    Initial commit

---
+++ src/components/Countries.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Cardformat from "./Cardformat";
+import "./Countries.css";
+
+function Countries() {
+  let [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    async function getCountriesData() {
+      try {
+        let rawData = await fetch(
+          "https://xcountries-backend.azurewebsites.net/all"
+        );
+        let finalData = await rawData.json();
+        setCountriesData(finalData);
+      } catch (error) {
+        console.log(`Error fetching data: ${error.message}`);
+      }
+    }
+
+    getCountriesData();
+  }, []);
+
+  return (
+    <div className="country-sec">
+      {countriesData.map((country) => (
+        <Cardformat name={country.name} flag={country.flag} />
+      ))}
+    </div>
+  );
+}
+
+export default Countries;
